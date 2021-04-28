8c796689280b058808613d0911aecc53ed804a85
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2021-04-28 09:14:48 +0200

    Added Tiny Browser

---
+++ TinyBrowser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Sockets;
+namespace gp20_2021_0426_rest_gameserver_Sopuffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
