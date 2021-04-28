326c8a09c3703ef3ba05a0b222631f1753d4db6e
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2021-04-28 13:07:30 +0200

    Fetched Acme Website. Converted it to bytes and wrote it in the stream.

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 namespace gp20_2021_0426_rest_gameserver_Sopuffer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpGetRequest("http://acme.com");
+            //Start();
+            Console.ReadKey();
+
+
         }
+
+        async static void HttpGetRequest(string website)
+        {
+            var tcpClient = new TcpClient("acme.com", 80);
+            var stream = tcpClient.GetStream();
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage responseMessage = await httpClient.GetAsync(website))
+                {
+                    using (HttpContent content = responseMessage.Content)
+                    {
+                        string contentMessage = await content.ReadAsStringAsync();
+
+                        Console.WriteLine(contentMessage);
+
+                        byte[] bytes = Encoding.ASCII.GetBytes(contentMessage);
+                        stream.Write(bytes);
+                    }
+                }
+            }
+        }
+        //static void Start()
+        //{
+        //    var tcpClient = new TcpClient("acme.com", 80);
+        //    var stream = tcpClient.GetStream();
+        //    // Get the bytes for our HTTP Request
+        //    var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
+        //    // Send them over TCP
+        //    stream.Write(bytes);
+        //}
+
+
     }
 }
