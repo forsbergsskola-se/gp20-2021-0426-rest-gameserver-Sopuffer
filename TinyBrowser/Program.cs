2f1b8ebb395e020eac2396049a4ee2bffb76e12a
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2021-04-28 14:43:40 +0200

    Removed and edited code

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,31 +1,25 @@
 ﻿using System;
-using System.Net;
 using System.Net.Sockets;
-using System.Collections.Generic;
 using System.Text;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.IO;
 namespace gp20_2021_0426_rest_gameserver_Sopuffer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // HttpGetRequest("http://acme.com");
             Start();
             Console.ReadKey();
-            
         }
         static void Start()
         {
             var tcpClient = new TcpClient("acme.com", 80);
             var stream = tcpClient.GetStream();
+
             var bytes = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
             stream.Write(bytes);
 
-            string test = Encoding.ASCII.GetString(bytes);
-            Console.WriteLine(test);
+            string httpRequestTitle = Encoding.ASCII.GetString(bytes);
+            Console.WriteLine(httpRequestTitle);
 
             byte[] resultBytes = new byte[124 * 124];
             var totalBytesReceived = 0;
@@ -49,7 +43,7 @@ namespace gp20_2021_0426_rest_gameserver_Sopuffer
             int Pos1 = content.IndexOf(STRFirst) + STRFirst.Length;
             int Pos2 = content.IndexOf(STRLast);
             string FinalString = content.Substring(Pos1, Pos2 - Pos1);
-            Console.WriteLine("Title: " + STRFirst + FinalString + STRLast);
+            Console.WriteLine("Title: "  + FinalString );
         }
         public static void FindHRef(string reference, string lastString, string text)
         {
@@ -61,43 +55,7 @@ namespace gp20_2021_0426_rest_gameserver_Sopuffer
             int Pos2 = content.IndexOf(STRLast);
           
         }
-        //async static void HttpGetRequest(string website)
-        //{
-        //    var tcpClient = new TcpClient("acme.com", 80);
-        //    var stream = tcpClient.GetStream();
 
-        //    using (HttpClient httpClient = new HttpClient())
-        //    {
-        //        using (HttpResponseMessage responseMessage = await httpClient.GetAsync(website))
-        //        {
-        //            using (HttpContent content = responseMessage.Content)
-        //            {
-        //                string contentMessage = await content.ReadAsStringAsync();
-        //                byte[] bytes = Encoding.ASCII.GetBytes(contentMessage);
-        //                stream.Write(bytes);
-
-        //                if(contentMessage.Contains("<title>") && contentMessage.Contains("</title>"))
-        //                {
-        //                    HttpGetTitle("<title>", "</title>", contentMessage);
-        //                }
-
-        //            }
-        //        }
-        //    }
-        //}
-
-        //async static void HttpGetTitle(string firstString, string lastString, string content)
-        //{
-
-        //    string text = content;
-        //    string STRFirst = "<title>";
-        //    string STRLast = "</title>";
-
-        //    int Pos1 = text.IndexOf(STRFirst) + STRFirst.Length;
-        //    int Pos2 = text.IndexOf(STRLast);
-        //    string FinalString = text.Substring(Pos1, Pos2 - Pos1);
-        //    Console.WriteLine("Title: " + STRFirst + FinalString + STRLast);
-        //}
 
 
     }
