2725e647dbffe639e8c7c39c460794a75956eebd
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2021-05-05 09:33:23 +0200

    Added GithubExplorer
    Added githubexplorer and can visit my github page.

---
+++ GithubExplorer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.Http;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace GithubExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GithubServer.Start();
+            Console.ReadLine();
+        }
+    }
+
+    class GithubServer
+    {
+        static readonly HttpClient client = new HttpClient();
+        public static async void Start()
+        {
+            Console.WriteLine("Please add a github Username to explore");
+            string input = Console.ReadLine();
+
+            if (input != null)
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://www.github.com/"+input);
+
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+        }
+    }
+}
