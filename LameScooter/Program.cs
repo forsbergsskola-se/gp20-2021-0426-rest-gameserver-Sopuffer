b5e17a35a298c65d4d502a184416fbd7e7a663ae
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2021-05-12 18:08:59 +0200

    Script can read through JSON and find stations

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text.Json;
 using System.IO;
 using Newtonsoft.Json;
     
@@ -12,7 +11,7 @@ namespace gp20_2021_0426_rest_gameserver_Sopuffer
         static async Task Main(string[] args)
         {
             OfflineLameScooterRental rental = new OfflineLameScooterRental();
-            var count = await rental.GetScooterCountInStation("Linnanmäki");
+            var count = await rental.GetScooterCountInStation("Orionintie");
             Console.WriteLine("Number of Scooters Available at this Station: " + count);
         }
     }
@@ -26,18 +25,16 @@ namespace gp20_2021_0426_rest_gameserver_Sopuffer
     {
         public async Task<int> GetScooterCountInStation(string stationName)
         {
-               var file = await File.ReadAllTextAsync("Scooters.json", System.Text.Encoding.ASCII);
+               var file = await File.ReadAllTextAsync("Scooters.json");
 
                 var jsonObject = JsonConvert.DeserializeObject<LameScooterStationList>(file);
 
                 foreach (var station in jsonObject.Stations)
                 {
-                    
                     if (station.name == stationName)
                     {
                          return station.bikesAvailable;
                     }
-                   Console.WriteLine(station.name + " " + station.bikesAvailable);
                 }
                return default;
             
@@ -50,20 +47,9 @@ namespace gp20_2021_0426_rest_gameserver_Sopuffer
     }
     public class LameScooterStation
     {
-        public string id { get; set; }
         public string name { get; set; }
-        public float x { get; set; }
-        public float y { get; set; }
         public int bikesAvailable { get; set; }
-        public int spacesAvailable { get; set; }
-        public int capacity { get; set; }
-        public bool allowDropoff { get; set; }
-        public bool allowOverloading { get; set; }
-        public bool isFloatingBike { get; set; }
-        public bool isCarStation { get; set; }
-        public string state { get; set; }
-        public string[] networks { get; set; }
-        public bool realTimeData { get; set; }
+     
     }
 }
 }
